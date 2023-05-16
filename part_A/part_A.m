7c1e9fdace7395f827994c341883d91675a79e04
Abdelrahman Ahmed <88385804+AbdelRahman119@users.noreply.github.com>
2023-05-16 08:47:51 +0300

    Update part_A.m

--- part_A/part_A.m
+++ part_A/part_A.m
@@ -13,5 +13,36 @@ function [N,no_of_cells,cell_radius,A_of_cell,A_of_sector]= part_A(GOS,City_area
   cell_area = City_area/no_of_cells;
   cell_radius = sqrt( 2*cell_area / ( 3*sqrt(3) ));
   A_of_cell = A_of_sector*no_of_sectors;
+  
+  %getting p_tx minimum by using link budget and getting the minimum reqired power at the cell radius
+  Path_Loss = losses(cell_radius);
+  p_tx_dBm =-95+Path_Loss ;
+  
+  %plotting prx in dBm vs distance in km.
+  d = [1:1:100];
+  p_rx = p_tx_dBm - losses(d);
+  
+  %drawing in normal scale 
+  subplot(2,1,1)
+  plot(d,p_rx)
+  grid on;
+  xlim([1 100]);
+  xlabel('Distance from reciever in km');
+  ylabel('MS recieved power in dBm');
+
+  %drawing in log scale 
+  subplot(2,1,2)
+  semilogx(d,p_rx)
+  grid on;
+  xlim([1 100]);
+  xlabel('Distance from reciever in km (log scale)');
+  ylabel('MS recieved power in dBm');
+
+end
+
+function Path_Loss = losses(d)
+%using Hata Model
+  CH = 0.8+(1.1*log10(900*(10^6))-0.7)*(1.5)-1.56*log10(900*(10^6));
+  Path_Loss = 69.55 + 26.16*log10(900*(10^6)) - 13.82*log10(20)- CH + log10(d)*(44.9-6.55*log10(20));
 
 end
