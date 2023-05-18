4592459d43b6652f57fc033822c2ab51a855cc4c
Abdelrahman Ahmed <88385804+AbdelRahman119@users.noreply.github.com>
2023-05-18 16:13:14 +0300

    Update part_A_interface.m

--- part_A/part_A_interface.m
+++ part_A/part_A_interface.m
@@ -8,3 +8,21 @@ sectorization_angle=input('Enter the sectorization angle(360 or 120 or 60) ');
 fprintf('cluster size is %d, number of cells is %d and cell radius is %.2f km \n',N,no_of_cells,cell_radius);
 fprintf('trafic intensity of a cell is %0.2f and trafic intensity of a sector is %.2f \n',A_of_cell,A_of_sector);
 fprintf('The required Base station transmitted power is %.2f dbm \n' ,p_tx_dBm);
+%plotting prx in dBm vs distance in km.
+d = [1:1:100];
+p_rx = p_tx_dBm - losses(d);
+
+%drawing in normal scale 
+subplot(2,1,1)
+plot(d,p_rx)
+grid on;
+xlim([1 100]);
+xlabel('Distance from reciever in km');
+ylabel('MS recieved power in dBm');
+%drawing in log scale 
+subplot(2,1,2)
+semilogx(d,p_rx)
+grid on;
+xlim([1 100]);
+xlabel('Distance from reciever in km (log scale)');
+ylabel('MS recieved power in dBm');
